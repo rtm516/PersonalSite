f324ee5d231774055a929f7c528333384ccb9404
rtm516 <ryantmilner@hotmail.co.uk>
2024-04-26 15:49:04 +0100

    Generate 404 for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()]
+  plugins: [svgr(), react(), copyDistFilePlugin({
+    input: 'index.html',
+    output: '404.html'
+  })]
 })
+
+function copyDistFilePlugin ({ input, output }) {
+  return {
+    name: 'copy-dist-file-plugin',
+    apply: 'build',
+    writeBundle (outputOptions) {
+      const inputPath = path.resolve(outputOptions.dir, input)
+      const outputPath = path.resolve(outputOptions.dir, output)
+
+      fs.copyFile(inputPath, outputPath, (err) => {
+        if (err) throw err
+      })
+    }
+  }
+}
