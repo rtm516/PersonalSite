853e1e9420fef4dd639a53d15632c47b63a148e4
rtm516 <ryantmilner@hotmail.co.uk>
2024-05-24 23:24:54 +0100

    Add description and fix header tag order

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,12 @@ function Home () {
       <div className='text-white h-[calc(100vh-4rem)]'>
         <SpipaCircle className='w-full h-[calc(100vh-4rem)] absolute' centerRadius={200} />
         <div className='relative w-full h-full flex flex-col items-center justify-center text-center'>
-          <h3 className='text-5xl font-bold'>
+          <h2 className='text-5xl font-bold'>
             Ryan Milner
-          </h3>
-          <h4 className='text-4xl font-italic'>
+          </h2>
+          <h3 className='text-4xl font-italic'>
             Penetration tester and developer
-          </h4>
+          </h3>
           <div className='flex justify-center flex-wrap gap-8 mt-6'>
             <Link to='/projects' className='rounded-lg font-bold py-4 px-12 bg-blue-700 hover:bg-blue-800 transition-colors duration-150'>
               Projects
